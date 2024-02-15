c485477543048dde3cb45e65da518a5c99c93808
anupama-pathirage <anupama@wso2.com>
2024-02-14 18:30:29 -0600

    Update readme

--- graphql-bank/main.bal
+++ graphql-bank/main.bal
@@ -3,31 +3,30 @@ import ballerina/graphql;
 service /bank on new graphql:Listener(9090) {
 
     resource function get accounts() returns Account[]|error {
-        Account acc1 = {
-            number: "123",
-            accType: "Savings",
-            holder: "John Doe",
-            address: "20A, FM Road, Austin, TX, 73301",
-            openedDate: "2020-01-01",
-            bankEmployee: {id: 1, name: "Mary Jones", position: "Operations Manager"}
-        };
-        Account[] accounts = [];
-        accounts.push(acc1);
-        return accounts;
+        return [
+            {
+                number: "123",
+                accType: "Savings",
+                holder: "John Doe",
+                address: "20A, FM Road, Austin, TX, 73301",
+                openedDate: "2020-01-01",
+                bankEmployee: {id: 1, name: "Mary Jones", position: "Operations Manager"}
+            }
+        ];
     }
 }
 
-type BankEmployee record {
+type BankEmployee record {|
     int id;
     string name;
     string position;
-};
+|};
 
-type Account record {
+type Account record {|
     string number;
     string accType;
     string holder;
     string address;
     string openedDate;
     BankEmployee bankEmployee;
-};
+|};
---
+++ resources/db/init.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS gq_bank_test;
+CREATE DATABASE gq_bank_test;
+USE gq_bank_test;
+CREATE TABLE Employees(employee_id INT AUTO_INCREMENT, name  VARCHAR(255), position VARCHAR(255), PRIMARY KEY (employee_id));
+CREATE TABLE Accounts(acc_number INT AUTO_INCREMENT, account_type VARCHAR(255), account_holder VARCHAR(255), address VARCHAR(255), opened_date VARCHAR(255), employee_id INT NOT NULL, PRIMARY KEY (acc_number), FOREIGN KEY (employee_id) REFERENCES Employees(employee_id));
+INSERT INTO Employees(name, position) VALUES ("Mary Jones", "Operations Manager");
+INSERT INTO Employees(name, position) VALUES ("Jennifer Davis", "Operations Manager");
+INSERT INTO Employees(name, position) VALUES ("John Smith", "Lead Manager");
+INSERT INTO Accounts(account_type, account_holder, address, opened_date, employee_id) VALUES ("Savings", "Robert Wilson", "101 Pine Road, Villagetown, TX 24680", "2022-10-01", (SELECT employee_id FROM Employees WHERE name="Mary Jones"));
+INSERT INTO Accounts(account_type, account_holder, address, opened_date, employee_id) VALUES ("FD", "Emily Johnson", "234 Maple Lane Suburbia, FL 13579", "2023-08-16", (SELECT employee_id FROM Employees WHERE name="Mary Jones"));
+INSERT INTO Accounts(account_type, account_holder, address, opened_date, employee_id) VALUES ("IRA", "Sarah Brown", "456 Elm Avenue Smalltown, CA 98765", "2010-10-22", (SELECT employee_id FROM Employees WHERE name="Jennifer Davis"));
+INSERT INTO Accounts(account_type, account_holder, address, opened_date, employee_id) VALUES ("Savings", "Lisa Martinez", "789 Oak Street Cityville, NY 54321", "2022-10-01", (SELECT employee_id FROM Employees WHERE name="John Smith"));
